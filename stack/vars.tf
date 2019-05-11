7e48f8238520fffde555338d28c06a62fa50d7f8
EC2 Default User <ec2-user@ip-172-31-41-67.us-east-2.compute.internal>
2019-05-11 17:11:13 +0000

    vars change

--- stack/vars.tf
+++ stack/vars.tf
@@ -7,7 +7,7 @@ variable "ami_web" {
 variable "ssh_key" {
   type = "string"
   description = "SSH Public Key"
-  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC4EQsnCjcG9DTrr5yG5ZiNSO2POdiknSphvvh+8pxdfADRlxg5fS3z4Q8CfuYdGqOXSoH4xWEpWOJVKEbFSsfM278MeyRnS7g1aUVT1VMWEPOtpmfBwN9FqMVctlHN8OdzGSY/31tuuTILtkPJ5pAFrPougpvECrJdd25WP/Z8KdpZecdccUPGJdh2etfjPOCScv3FxoGMgeZUenuhkLTsKbvHopGUpwIlLfVn6Ke+OOZheg4rk2fCaCh6pK9Y83jMasbyqkNsRI+ag3jhJBXOQ1WkSVR6HyItWPo1/JZevGP00Iqmrey7Pq/9JkE0SHWy0PBg5K5LyOvBsEzLtOuN richaws"
+  default = "ssh-rsa  richaws"
 }
 
 variable "instance_type_web" {
